570e33a0a09541304967f43d35f86aee875ec2bf
shinyay <shinya.com@gmail.com>
2023-04-12 18:30:55 +0900

    Add Book entity

--- src/main/kotlin/com/example/demo/entity/Book.kt
+++ src/main/kotlin/com/example/demo/entity/Book.kt
@@ -1,4 +1,14 @@
 package com.example.demo.entity
 
-class Book {
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+
+@Entity
+class Book(var name: String?, var author: String?) {
+    @Id
+    @GeneratedValue
+    var id: Long? = null
+    
+    constructor() : this("", "")
 }
