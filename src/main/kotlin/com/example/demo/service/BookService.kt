02b6ad422a8529d2df4b2ad6baff39b3aa8a006a
shinyay <shinya.com@gmail.com>
2023-04-13 10:51:28 +0900

    Add a initilize data

--- src/main/kotlin/com/example/demo/service/BookService.kt
+++ src/main/kotlin/com/example/demo/service/BookService.kt
@@ -1,4 +1,33 @@
 package com.example.demo.service
 
-class BookService {
+import com.example.demo.entity.Book
+import com.example.demo.repository.BookRepository
+import jakarta.annotation.PostConstruct
+import org.springframework.stereotype.Service
+
+@Service
+class BookService(val repository: BookRepository) {
+
+    @PostConstruct
+    fun initSampleData() {
+        repository.save(
+            Book(
+                "Learning Spring Boot 3.0",
+                "Greg L. Turnquist"
+            )
+        )
+        repository.save(
+            Book(
+                "Spring in Action",
+                "Craig Walls")
+        )
+        repository.save(
+            Book(
+                "Spring Boot: Up and Running",
+                "Mark Heckler"
+            )
+        )
+    }
+
+    fun getBooks() = repository.findAll()
 }
