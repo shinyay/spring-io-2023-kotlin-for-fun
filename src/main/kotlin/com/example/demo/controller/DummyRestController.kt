a0fe52d95e141676cb03c9a9960abcf6900430e6
shinyay <shinya.com@gmail.com>
2023-04-13 11:05:09 +0900

    Add a validation class with Rest Contrller

--- src/main/kotlin/com/example/demo/controller/DummyRestController.kt
+++ src/main/kotlin/com/example/demo/controller/DummyRestController.kt
@@ -1,4 +1,13 @@
 package com.example.demo.controller
 
-class DummyRestController {
+import com.example.demo.entity.Book
+import com.example.demo.service.BookService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class DummyRestController(val service: BookService) {
+
+    @GetMapping("/")
+    fun validateOperationListData(): MutableList<Book> = service.getBooks()
 }
