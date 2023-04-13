9799f9b72198c2e308074b6d1397c7b0edecb1d8
shinyay <shinya.com@gmail.com>
2023-04-13 13:37:16 +0900

    Add a function to redirect to Index page

--- src/main/kotlin/com/example/demo/controller/BookController.kt
+++ src/main/kotlin/com/example/demo/controller/BookController.kt
@@ -1,8 +1,17 @@
 package com.example.demo.controller
 
+import com.example.demo.service.BookService
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class BookController {
+class BookController(val service: BookService) {
 
+    @GetMapping("/")
+    fun index(model: Model): String {
+        model.addAttribute("books",service.getBooks())
+
+        return "index"
+    }
 }
