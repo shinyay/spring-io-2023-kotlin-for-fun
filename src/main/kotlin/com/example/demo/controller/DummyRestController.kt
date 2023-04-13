9f1efc7e1884583792687a97fd37241d174851ce
shinyay <shinya.com@gmail.com>
2023-04-13 11:06:16 +0900

    Add a endpoint with RequestMapping

--- src/main/kotlin/com/example/demo/controller/DummyRestController.kt
+++ src/main/kotlin/com/example/demo/controller/DummyRestController.kt
@@ -3,11 +3,13 @@ package com.example.demo.controller
 import com.example.demo.entity.Book
 import com.example.demo.service.BookService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/api/v1")
 class DummyRestController(val service: BookService) {
 
-    @GetMapping("/")
+    @GetMapping("/books")
     fun validateOperationListData(): MutableList<Book> = service.getBooks()
 }
