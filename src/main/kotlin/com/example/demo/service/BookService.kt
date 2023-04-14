5e1c90e3b530facbb43c3bfa633cb58320378c64
shinyay <shinya.com@gmail.com>
2023-04-14 14:19:30 +0900

    Midify custom finder for QBE, Query by Example

--- src/main/kotlin/com/example/demo/service/BookService.kt
+++ src/main/kotlin/com/example/demo/service/BookService.kt
@@ -2,8 +2,11 @@ package com.example.demo.service
 
 import com.example.demo.dto.SearchBook
 import com.example.demo.entity.Book
+import com.example.demo.logger
 import com.example.demo.repository.BookRepository
 import jakarta.annotation.PostConstruct
+import org.springframework.data.domain.Example
+import org.springframework.data.domain.ExampleMatcher
 import org.springframework.stereotype.Service
 import org.springframework.util.StringUtils
 import java.util.*
@@ -35,24 +38,38 @@ class BookService(val repository: BookRepository) {
     fun getBooks(): MutableList<Book> = repository.findAll()
 
     fun search(searchBook: SearchBook): MutableList<Book> {
-        if (StringUtils.hasText(searchBook.name) && StringUtils.hasText(searchBook.author)) {
-            return repository.findByNameContainsOrAuthorContainsAllIgnoreCase(
-                searchBook.name, searchBook.author
-            )
+        val probe = Book()
+
+        if (StringUtils.hasText(searchBook.value)) {
+            probe.name = searchBook.value
+            probe.author = searchBook.value
         }
 
-        if (StringUtils.hasText(searchBook.name)) {
-            return repository.findByNameContainsIgnoreCase(
-                searchBook.name
-            )
-        }
-
-        if (StringUtils.hasText(searchBook.author)) {
-            return repository.findByAuthorContainsIgnoreCase(
-                searchBook.author
-            )
-        }
-
-        return Collections.emptyList()
+        val example: Example<Book> = Example.of(
+            probe,
+            ExampleMatcher.matchingAny()
+                .withIgnoreCase()
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING)
+        )
+        return repository.findAll(example)
+//        if (StringUtils.hasText(searchBook.name) && StringUtils.hasText(searchBook.author)) {
+//            return repository.findByNameContainsOrAuthorContainsIgnoreCase(
+//                searchBook.name, searchBook.author
+//            )
+//        }
+//
+//        if (StringUtils.hasText(searchBook.name)) {
+//            return repository.findByNameContainsIgnoreCase(
+//                searchBook.name
+//            )
+//        }
+//
+//        if (StringUtils.hasText(searchBook.author)) {
+//            return repository.findByAuthorContainsIgnoreCase(
+//                searchBook.author
+//            )
+//        }
+//
+//        return Collections.emptyList()
     }
 }
