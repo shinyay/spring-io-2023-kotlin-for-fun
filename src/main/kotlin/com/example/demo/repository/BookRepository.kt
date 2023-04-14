c1a5de7e8edfec4a08ba0ec35fb26f03807ad355
shinyay <shinya.com@gmail.com>
2023-04-14 14:16:13 +0900

    Modify function definition to remove the useless

--- src/main/kotlin/com/example/demo/repository/BookRepository.kt
+++ src/main/kotlin/com/example/demo/repository/BookRepository.kt
@@ -4,16 +4,16 @@ import com.example.demo.entity.Book
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface BookRepository : JpaRepository<Book, Long> {
-    fun findByNameContainsOrAuthorContainsAllIgnoreCase(
-        name: String,
-        author: String
-    ): MutableList<Book>
-
-    fun findByNameContainsIgnoreCase(
-        name: String
-    ): MutableList<Book>
-
-    fun findByAuthorContainsIgnoreCase(
-        author: String
-    ): MutableList<Book>
+//    fun findByNameContainsOrAuthorContainsIgnoreCase(
+//        name: String,
+//        author: String
+//    ): MutableList<Book>
+//
+//    fun findByNameContainsIgnoreCase(
+//        name: String
+//    ): MutableList<Book>
+//
+//    fun findByAuthorContainsIgnoreCase(
+//        author: String
+//    ): MutableList<Book>
 }
