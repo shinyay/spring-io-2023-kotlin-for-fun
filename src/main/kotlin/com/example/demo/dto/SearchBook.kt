0860e38bc37926cd7f3b26da7f1afacf193fbdce
shinyay <shinya.com@gmail.com>
2023-04-14 14:15:14 +0900

    Modify field for using Example QBE(Query by Example)

--- src/main/kotlin/com/example/demo/dto/SearchBook.kt
+++ src/main/kotlin/com/example/demo/dto/SearchBook.kt
@@ -1,3 +1,4 @@
 package com.example.demo.dto
 
-data class SearchBook(val name: String, val author: String)
+//data class SearchBook(val name: String, val author: String)
+data class SearchBook(val value: String)
