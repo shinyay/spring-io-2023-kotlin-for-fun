fc97ff3597c0acbe4446d1e814fbee850f869a49
shinyay <shinya.com@gmail.com>
2023-04-15 11:25:32 +0900

    Modify the code to optimize

--- src/main/kotlin/com/example/demo/controller/BookController.kt
+++ src/main/kotlin/com/example/demo/controller/BookController.kt
@@ -1,11 +1,13 @@
 package com.example.demo.controller
 
+import com.example.demo.dto.NewBook
 import com.example.demo.dto.SearchBook
 import com.example.demo.service.BookService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 
 @Controller
@@ -23,5 +25,17 @@ class BookController(val service: BookService) {
         val searchResult = service.search(searchBook)
         model.addAttribute("books", searchResult)
         return "index"
+    }
+
+    @PostMapping("/book/delete/{id}")
+    fun delete(@PathVariable id: Long): String {
+        service.delete(id)
+        return "redirect:/"
+    }
+
+    @PostMapping("/book/register")
+    fun register(@ModelAttribute book: NewBook): String {
+        service.register(book)
+        return "redirect:/"
     }
 }
--- src/main/kotlin/com/example/demo/repository/BookRepository.kt
+++ src/main/kotlin/com/example/demo/repository/BookRepository.kt
@@ -3,17 +3,4 @@ package com.example.demo.repository
 import com.example.demo.entity.Book
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface BookRepository : JpaRepository<Book, Long> {
-//    fun findByNameContainsOrAuthorContainsIgnoreCase(
-//        name: String,
-//        author: String
-//    ): MutableList<Book>
-//
-//    fun findByNameContainsIgnoreCase(
-//        name: String
-//    ): MutableList<Book>
-//
-//    fun findByAuthorContainsIgnoreCase(
-//        author: String
-//    ): MutableList<Book>
-}
+interface BookRepository : JpaRepository<Book, Long>
